a143aee7bc27c2c47b5b263a32553245f3077b92
Fotis Athineos <fotis@transifex.com>
2024-01-10 17:27:06 +0200

    Export services to csv

--- src/gt/vehicle/admin.py
+++ src/gt/vehicle/admin.py
@@ -1,8 +1,15 @@
+from csv import writer
+from datetime import datetime
+from io import BytesIO, StringIO
+from typing import Tuple
+from zipfile import ZipFile
+from django.http import HttpResponse
 from django.contrib import admin
 from django.db.models import Max
 from django.utils.translation import gettext_lazy as _
 from gt.vehicle.models import Vehicle, VehicleOwner, VehicleOwnership
 from gt.service.admin import ServiceInline
+from gt.service.models import Service
 
 
 admin.site.disable_action('delete_selected')
@@ -29,7 +36,6 @@ class VehicleOwnershipInline(admin.StackedInline):
 
 @admin.register(Vehicle)
 class VehicleAdmin(ModelAdmin):
-    actions = []
     inlines = (ServiceInline, VehicleOwnershipInline,)
     list_display = ('plate_number', 'manufacturer', 'last_service_date', 'total_service_cost', 'last_service_travel_distance')
     search_fields = ('plate_number', 'manufacturer',
@@ -58,6 +64,57 @@ class VehicleAdmin(ModelAdmin):
 
     def last_service_date(self, obj):
         return obj.last_service_date
+
+    def export_csv(self, _, queryset):
+        if not queryset.count():
+            return
+
+        csv_files = [
+            self._generate_vehicle_csv(v) for v in queryset for v in queryset.all()
+        ]
+        filename = "{}.{}".format(
+            datetime.now().strftime('%Y%m%d-%H%m'),
+            'zip'
+        )
+        zip_buffer = BytesIO()
+        with ZipFile(zip_buffer, 'w') as zipf:
+            for file_name, csv_file in csv_files:
+                zipf.writestr(file_name, csv_file.getvalue())
+                csv_file.close()
+
+        response = HttpResponse(zip_buffer.getvalue(), content_type='application/zip')
+        response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+        return response
+
+    @classmethod
+    def _generate_vehicle_csv(cls, vehicle) -> Tuple[str, StringIO]:
+        filename = "{}-{}.{}".format(
+            vehicle.plate_number.lower(),
+            datetime.now().strftime('%Y%m%d-%H%m'),
+            'csv'
+        )
+        csv = StringIO()
+        csv_writer = writer(csv)
+        headers = []
+        for field in Service._meta.fields:
+            headers.append(field.verbose_name)
+        csv_writer.writerow(headers)
+
+        for service in vehicle.service_set.order_by('-service_date'):
+            row = []
+            for field in Service._meta.fields:
+                value = getattr(service, field.name)
+                if callable(value):
+                    value = value()
+                row.append(value)
+            csv_writer.writerow(row)
+
+        return (filename, csv)
+
+
+    export_csv.short_description = "Export to CSV"
+    actions = [export_csv]
 
 
 @admin.register(VehicleOwner)
